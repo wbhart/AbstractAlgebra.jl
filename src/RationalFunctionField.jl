d413833625bc8361f799677bba7e739f8c07921d
William Hart <goodwillhart@googlemail.com>
2021-08-25 17:26:51 +0200

    Clean up RationalFunctionField.

--- src/RationalFunctionField.jl
+++ src/RationalFunctionField.jl
@@ -1,14 +1,6 @@
 export RationalFunctionField
 
-function RationalFunctionField(k::Field, s::Symbol; cached=true)
-   return Generic.RationalFunctionField(k, s; cached=cached)
-end
-
-function RationalFunctionField(k::Field, s::Char; cached=true)
-   return Generic.RationalFunctionField(k, Symbol(s); cached=cached)
-end
-
-function RationalFunctionField(k::Field, s::AbstractString; cached=true)
+function RationalFunctionField(k::Field, s::String; cached=true)
    return Generic.RationalFunctionField(k, Symbol(s); cached=cached)
 end
 
