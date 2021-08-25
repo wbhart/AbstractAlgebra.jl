a8a7a7b1a9fbfcb8363d9c9b171e3372f8cc517d
William Hart <goodwillhart@googlemail.com>
2021-08-25 16:17:22 +0200

    Trim Fraction.jl.

--- src/Fraction.jl
+++ src/Fraction.jl
@@ -1,16 +1,4 @@
-###############################################################################
-#
-#   Fraction.jl : fraction fields
-#
-###############################################################################
-
 export FractionField
-
-###############################################################################
-#
-#   Data type and parent object methods
-#
-###############################################################################
 
 base_ring(a::FracField{T}) where T <: RingElem = a.base_ring::parent_type(T)
 
@@ -26,26 +14,11 @@ function isexact_type(a::Type{T}) where {S <: RingElement, T <: FracElem{S}}
    return isexact_type(S)
 end
 
-@doc Markdown.doc"""
-    characteristic(R::FracField{T}) where T <: RingElem
-
-Return the characteristic of the given field.
-"""
-function characteristic(R::FracField{T}) where T <: RingElem
-   return characteristic(base_ring(R))
-end
-
 function check_parent(a::FracElem, b::FracElem, throw::Bool = true)
    fl = parent(a) != parent(b)
    fl && throw && error("Incompatible rings in fraction field operation")
    return !fl
 end
-
-###############################################################################
-#
-#   Constructors
-#
-###############################################################################
 
 function //(x::T, y::T) where {T <: RingElem}
    R = parent(x)
@@ -64,19 +37,12 @@ end
 
 //(x::FracElem{T}, y::T) where {T <: RingElem} = x//parent(x)(y)
 
-###############################################################################
-#
-#   Basic manipulation
-#
-###############################################################################
-
 function Base.hash(a::FracElem, h::UInt)
    b = 0x8a30b0d963237dd5%UInt
    # We canonicalise before hashing
    return xor(b, hash(numerator(a, true), h), hash(denominator(a, true), h), h)
 end
 
-# Fall back method for all other fraction types in system
 function Base.numerator(a::FracElem, canonicalise::Bool=true)
    return Base.numerator(a) # all other types ignore canonicalise
 end
@@ -94,59 +60,11 @@ iszero(a::FracElem) = iszero(numerator(a, false))
 
 isone(a::FracElem) = numerator(a, false) == denominator(a, false)
 
-isunit(a::FracElem) = !iszero(numerator(a, false))
-
-###############################################################################
-#
-#   Canonicalisation
-#
-###############################################################################
-
 canonical_unit(a::FracElem) = a
-
-###############################################################################
-#
-#   AbstractString I/O
-#
-###############################################################################
-
-function expressify(a::FracElem; context = nothing)
-    n = numerator(a, true)
-    d = denominator(a, true)
-    if isone(d)
-        return expressify(n)
-    else
-        return Expr(:call, ://, expressify(n), expressify(d))
-    end
-end
-
-function show(io::IO, ::MIME"text/plain", a::FracElem)
-  print(io, obj_to_string(a, context = io))
-end
-
-function show(io::IO, a::FracElem)
-  print(io, obj_to_string(a, context = io))
-end
-
-function show(io::IO, a::FracField)
-   print(IOContext(io, :compact => true), "Fraction field of ", base_ring(a))
-end
-
-###############################################################################
-#
-#   Unary operators
-#
-###############################################################################
 
 function -(a::FracElem)
    return parent(a)(-numerator(a, false), deepcopy(denominator(a, false)))
 end
-
-###############################################################################
-#
-#   Binary operators
-#
-###############################################################################
 
 function +(a::FracElem{T}, b::FracElem{T}) where {T <: RingElem}
    check_parent(a, b)
@@ -286,12 +204,6 @@ function *(a::FracElem{T}, b::FracElem{T}) where {T <: RingElem}
    return parent(a)(n, d)
 end
 
-###############################################################################
-#
-#   Ad hoc binary operators
-#
-###############################################################################
-
 function *(a::FracElem, b::Union{Integer, Rational, AbstractFloat})
    c = base_ring(a)(b)
    g = gcd(denominator(a, false), c)
@@ -362,19 +274,6 @@ function -(a::T, b::FracElem{T}) where {T <: RingElem}
    return parent(b)(n, deepcopy(d))
 end
 
-###############################################################################
-#
-#   Comparisons
-#
-###############################################################################
-
-@doc Markdown.doc"""
-    ==(x::FracElem{T}, y::FracElem{T}) where {T <: RingElem}
-
-Return `true` if $x == y$ arithmetically, otherwise return `false`. Recall
-that power series to different precisions may still be arithmetically
-equal to the minimum of the two precisions.
-"""
 function ==(x::FracElem{T}, y::FracElem{T}) where {T <: RingElem}
    b  = check_parent(x, y, false)
    !b && return false
@@ -387,86 +286,27 @@ function ==(x::FracElem{T}, y::FracElem{T}) where {T <: RingElem}
            denominator(x, false)*numerator(y, false))
 end
 
-@doc Markdown.doc"""
-    isequal(x::FracElem{T}, y::FracElem{T}) where {T <: RingElem}
-
-Return `true` if $x == y$ exactly, otherwise return `false`. This function is
-useful in cases where the numerators and denominators of the fractions are
-inexact, e.g. power series. Only if the power series are precisely the same,
-to the same precision, are they declared equal by this function.
-"""
-function isequal(x::FracElem{T}, y::FracElem{T}) where {T <: RingElem}
-   if parent(x) != parent(y)
-      return false
-   end
-   return isequal(numerator(x, false)*denominator(y, false),
-                  denominator(x, false)*numerator(y, false))
-end
-
-###############################################################################
-#
-#   Ad hoc comparisons
-#
-###############################################################################
-
-@doc Markdown.doc"""
-    ==(x::FracElem, y::Union{Integer, Rational, AbstractFloat})
-
-Return `true` if $x == y$ arithmetically, otherwise return `false`.
-"""
 function ==(x::FracElem, y::Union{Integer, Rational, AbstractFloat})
    return (isone(denominator(x, false)) && numerator(x, false) == y) ||
           (isone(denominator(x, true)) && numerator(x, true) == y) ||
           (numerator(x, false) == denominator(x, false)*y)
 end
 
-@doc Markdown.doc"""
-    ==(x::Union{Integer, Rational, AbstractFloat}, y::FracElem)
-
-Return `true` if $x == y$ arithmetically, otherwise return `false`.
-"""
 ==(x::Union{Integer, Rational, AbstractFloat}, y::FracElem) = y == x
 
-@doc Markdown.doc"""
-    ==(x::FracElem{T}, y::T) where {T <: RingElem}
-
-Return `true` if $x == y$ arithmetically, otherwise return `false`.
-"""
 function ==(x::FracElem{T}, y::T) where {T <: RingElem}
    return (isone(denominator(x, false)) && numerator(x, false) == y) ||
           (isone(denominator(x, true)) && numerator(x, true) == y) ||
           (numerator(x, false) == denominator(x, false)*y)
 end
 
-@doc Markdown.doc"""
-    ==(x::T, y::FracElem{T}) where {T <: RingElem}
-
-Return `true` if $x == y$ arithmetically, otherwise return `false`.
-"""
 ==(x::T, y::FracElem{T}) where {T <: RingElem} = y == x
 
-###############################################################################
-#
-#   Inversion
-#
-###############################################################################
-
-@doc Markdown.doc"""
-    Base.inv(a::FracElem)
-
-Return the inverse of the fraction $a$.
-"""
 function Base.inv(a::FracElem)
    iszero(numerator(a, false)) && throw(DivideError())
    return parent(a)(deepcopy(denominator(a, false)),
                     deepcopy(numerator(a, false)))
 end
-
-###############################################################################
-#
-#   Exact division
-#
-###############################################################################
 
 function divexact(a::FracElem{T}, b::FracElem{T}; check::Bool=true) where {T <: RingElem}
    check_parent(a, b)
@@ -512,22 +352,6 @@ function divexact(a::FracElem{T}, b::FracElem{T}; check::Bool=true) where {T <: 
    return parent(a)(n, d)
 end
 
-function divides(a::FracElem{T}, b::FracElem{T}) where {T <: RingElem}
-   if iszero(a)
-     return true, parent(a)()
-   end
-   if iszero(b)
-     return false, parent(a)()
-   end
-   return true, divexact(a, b)
-end
-
-###############################################################################
-#
-#   Ad hoc exact division
-#
-###############################################################################
-
 function divexact(a::FracElem, b::Union{Integer, Rational, AbstractFloat}; check::Bool=true)
    b == 0 && throw(DivideError())
    c = base_ring(a)(b)
@@ -562,30 +386,6 @@ function divexact(a::T, b::FracElem{T}; check::Bool=true) where {T <: RingElem}
    return parent(b)(n, d)
 end
 
-##############################################################################
-#
-#  Evaluation
-#
-##############################################################################
-
-function evaluate(f::FracElem{T}, V::Vector{U}) where {T <: RingElement, U <: RingElement}
-    return evaluate(numerator(f), V)//evaluate(denominator(f), V)
-end
-  
-function evaluate(f::FracElem{T}, v::U) where {T <: RingElement, U <: RingElement}
-    return evaluate(numerator(f), v)//evaluate(denominator(f), v)
-end
-
-function evaluate(f::FracElem{T}, v::U) where {T <: PolyElem, U <: Integer}
-    return evaluate(numerator(f), v)//evaluate(denominator(f), v)
-end
- 
-###############################################################################
-#
-#   Powering
-#
-###############################################################################
-
 function ^(a::FracElem{T}, b::Int) where {T <: RingElem}
    if b < 0
       a = inv(a)
@@ -594,57 +394,6 @@ function ^(a::FracElem{T}, b::Int) where {T <: RingElem}
    return parent(a)(numerator(a)^b, denominator(a)^b)
 end
 
-##############################################################################
-#
-#  Derivative
-#
-##############################################################################
-
-function derivative(f::FracElem{T}) where {T <: PolyElem}
-    n = numerator(f)
-    d = denominator(f)
-    return (derivative(n)*d - n*derivative(d))//d^2
-end
-
-###############################################################################
-#
-#   Square root
-#
-###############################################################################
-
-@doc Markdown.doc"""
-    issquare(a::FracElem{T}) where T <: RingElem
-
-Return `true` if $a$ is a square.
-"""
-function issquare(a::FracElem{T}) where T <: RingElem
-   return issquare(numerator(a)) && issquare(denominator(a))
-end
-
-@doc Markdown.doc"""
-    Base.sqrt(a::FracElem{T}; check::Bool=true) where T <: RingElem
-
-Return the square root of $a$. By default the function will throw an
-exception if the input is not square. If `check=false` this test is omitted.
-"""
-function Base.sqrt(a::FracElem{T}; check::Bool=true) where T <: RingElem
-   return parent(a)(sqrt(numerator(a); check=check), sqrt(denominator(a); check=check))
-end
-
-###############################################################################
-#
-#   GCD
-#
-###############################################################################
-
-@doc Markdown.doc"""
-    gcd(a::FracElem{T}, b::FracElem{T}) where {T <: RingElem}
-
-Return a greatest common divisor of $a$ and $b$ if one exists. N.B: we define
-the GCD of $a/b$ and $c/d$ to be gcd$(ad, bc)/bd$, reduced to lowest terms.
-This requires the existence of a greatest common divisor function for the
-base ring.
-"""
 function gcd(a::FracElem{T}, b::FracElem{T}) where {T <: RingElem}
    check_parent(a, b)
    gbd = gcd(denominator(a, false), denominator(b, false))
@@ -657,43 +406,6 @@ function gcd(a::FracElem{T}, b::FracElem{T}) where {T <: RingElem}
    d = divexact(d, canonical_unit(d))
    return parent(a)(n, d)
 end
-
-################################################################################
-#
-#   Remove and valuation
-#
-################################################################################
-
-@doc Markdown.doc"""
-    remove(z::FracElem{T}, p::T) where {T <: RingElem}
-
-Return the tuple $n, x$ such that $z = p^nx$ where $x$ has valuation $0$ at
-$p$.
-"""
-function remove(z::FracElem{T}, p) where {T}
-   p = convert(T, p)
-   iszero(z) && error("Not yet implemented")
-   v, d = remove(denominator(z, false), p)
-   w, n = remove(numerator(z, false), p)
-   return w-v, parent(z)(deepcopy(n), deepcopy(d))
-end
-
-@doc Markdown.doc"""
-    valuation(z::FracElem{T}, p::T) where {T <: RingElem}
-
-Return the valuation of $z$ at $p$.
-"""
-function valuation(z::FracElem{T}, p) where {T}
-   p = convert(T, p)
-   v, _ = remove(z, p)
-   return v
-end
-
-###############################################################################
-#
-#   Unsafe operators and functions
-#
-###############################################################################
 
 function zero!(c::FracElem)
    c.num = zero!(c.num)
@@ -844,54 +556,6 @@ function add!(c::FracElem{T}, a::FracElem{T}, b::FracElem{T}) where {T <: RingEl
    return c
 end
 
-###############################################################################
-#
-#   Random functions
-#
-###############################################################################
-
-RandomExtensions.maketype(R::FracField, _) = elem_type(R)
-
-function RandomExtensions.make(S::FracField, vs...)
-   R = base_ring(S)
-   if length(vs) == 1 && elem_type(R) == Random.gentype(vs[1])
-      RandomExtensions.Make(S, vs[1]) # forward to default Make constructor
-   else
-      make(S, make(R, vs...))
-   end
-end
-
-function rand(rng::AbstractRNG,
-              sp::SamplerTrivial{<:Make2{<:RingElement, <:FracField}})
-   S, v = sp[][1:end]
-   R = base_ring(S)
-   n = rand(rng, v)
-   d = R()
-   while iszero(d)
-      d = rand(rng, v)
-   end
-   return S(n, d)
-end
-
-rand(rng::AbstractRNG, S::FracField, v...) =
-   rand(rng, make(S, v...))
-
-rand(S::FracField, v...) = rand(GLOBAL_RNG, S, v...)
-
-###############################################################################
-#
-#   FractionField constructor
-#
-###############################################################################
-
-@doc Markdown.doc"""
-    FractionField(R::Ring; cached=true)
-
-Return the parent object of the fraction field over the given base ring $R$.
-If `cached == true` (the default), the returned parent object is cached so
-that it will always be returned by a call to the constructor when the same
-base ring $R$ is supplied.
-"""
 function FractionField(R::Ring; cached=true)
    return Generic.FractionField(R; cached=cached)
 end
